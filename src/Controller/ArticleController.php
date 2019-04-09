b611254bb05d173128faa4b17ccd6dfb9595e7fe
dds <dds@gmail.com>
2019-04-09 14:43:25 +0200

    Sf4 perf Day 2 Add Edit Delete Article + validator

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -44,10 +44,50 @@ class ArticleController extends AbstractController
     }
 
     /**
+     * @Route("/article/{slug}/editarticle", name="app_editarticle", methods={"GET","POST"})
+     * @param Article $article
+     * @param Request $request
      * @return Response
      */
-    public function edit(): Response
+    public function edit(Article $article, Request $request): Response
     {
-        return $this->render('article/udpate.html.twig');
+        //récupération du formulaire
+        $form = $this->createForm(ArticleType::class, $article);
+        //remplir le formulaire avec les variables $_POST
+        $form->handleRequest($request);
+
+        //On vérifie que le formulaire est soumis et validé
+        if ($form->isSubmitted() && $form->isValid()) {
+            //récupération du manager
+            $entityManager = $this->getDoctrine()->getManager();
+            // Maj de la bdd
+            $entityManager->flush();
+            // Ajout du message flash
+            $this->addFlash('success', 'Votre article a bien été modifié !');
+
+            /*return $this->redirectToRoute('app_home');*/
+            return $this->redirectToRoute('app_show', ['slug' => $article->getSlug()]);
+        }
+
+
+        return $this->render('article/udpate.html.twig', [
+            'editForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route ("/article/{slug}/delete", name= "app_delete")
+     * @param Article $article
+     * @return Response
+     */
+    public function delete(Article $article): Response
+    {
+        //Récupération du manager
+        $manager = $this->getDoctrine()->getManager();
+        //Suppression de l'article
+        $manager->remove($article);
+        $manager->flush();
+        //Redirection de la liste des articles
+        return $this->redirectToRoute('app_home');
     }
 }
